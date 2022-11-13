1aa5be6914fbf805a4a6d6ca4679e4cdf1e7789d
KlimovIA <rogmat.dniwe.i@gmail.com>
2022-11-13 19:33:58 +0300

    Инструменты для отладки
    По запаре не добавленные в репозиторий версии 0.0.4

---
+++ CommonTypes/Data/Debug/DebugConsole.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteManager.CommonTypes.Data.Debug
+{
+    internal static class DebugConsole
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool AllocConsole();
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool FreeConsole();              
+    }
+}
---
+++ CommonTypes/Data/Debug/Logger.cs
@@ -0,0 +1,22 @@
+﻿namespace NoteManager.CommonTypes.Data.Debug
+{
+    internal static class Logger
+    {
+        /// <summary>
+        /// Логгирование ошибок.
+        /// </summary>
+        /// <param name="className"> Наименование класса, вызывающего логгер. </param>
+        /// <param name="Message"> Описание ошибки </param>
+        public static async void WriteLogMessage(string className, string Message)
+        {
+            string formatedLogMessage = $"[{DateTime.Now}] >> ClassName: {className}; Message: {Message}";
+            await Task.Run(() =>
+            {
+                using (StreamWriter writer = new StreamWriter("Log.txt", true))
+                {
+                    writer.WriteLine(formatedLogMessage);
+                }
+            });
+        }
+    }
+}
